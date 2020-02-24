3030c376a71b41e84dcbcacadc79249e0507c717
Andrew <Andrew@DESKTOP-1UPPPHP>
2020-02-23 23:43:43 -0600

    documentation

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -25,77 +25,166 @@ namespace PointOfSale
             //AngreeChicken.Height = 20;
         }
 
+        /// <summary>
+        /// when Angree chicken clicked add AngreeChicken to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AngreeChicken_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new AngryChicken());
         }
 
+
+        /// <summary>
+        /// when CowpokeChili clicked add CowpokeChili to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CowpokeChilli_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new CowpokeChili());
         }
+
+        /// <summary>
+        /// when TexasTripleBurger clicked add TexasTripleBurger to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 
         private void TexasTripleBurger_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new TexasTripleBurger());
         }
 
+
+        /// <summary>
+        /// when PecosPulledPork clicked add PecosPulledPork to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PecosPulledPork_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new PecosPulledPork());
         }
 
+
+        /// <summary>
+        /// when RustlersRibs clicked add RustlersRibs to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void RustlersRibs_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new RustlersRibs());
         }
 
+
+        /// <summary>
+        /// when DakotaDoubleBurger clicked add DakotaDoubleBurger to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void DakotaDoubleBurger_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new DakotaDoubleBurger());
         }
 
+
+        /// <summary>
+        /// when TrailBurger clicked add TrailBurger to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TrailBurger_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new TrailBurger());
         }
 
+
+        /// <summary>
+        /// when ChiliCheeseFries clicked add ChiliCheeseFries to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ChiliCheeseFries_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new ChiliCheeseFries());
         }
 
+
+        /// <summary>
+        /// when BakedBeans clicked add BakedBeans to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BakedBeans_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new BakedBeans());
         }
 
+
+        /// <summary>
+        /// when CowboyCoffee clicked add CowboyCoffee to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CowboyCoffee_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new CowboyCoffee());
         }
 
+
+        /// <summary>
+        /// when Water clicked add Water to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Water_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new Water());
         }
 
+
+        /// <summary>
+        /// when JerkedSoda clicked add JerkedSoda to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void JerkedSoda_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new JerkedSoda());
         }
 
+
+        /// <summary>
+        /// when TexasTea clicked add TexasTea to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TexasTea_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new TexasTea());
         }
 
+
+        /// <summary>
+        /// when PanDeCampo clicked add PanDeCampo to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PanDeCampo_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new PanDeCampo());
 
         }
 
+
+        /// <summary>
+        /// whenCornDodgers clicked add CornDodgers to ticket
+        /// /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CornDodgers_Click(object sender, RoutedEventArgs e)
         {
             OrderList.Items.Add(new CornDodgers());
