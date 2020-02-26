1071646658c0641368b681f86607af756dff956a
aphanlin <aphanlin@F1092-23-2111>
2020-02-26 17:14:21 -0600

    order classs

--- Data/Order.cs
+++ Data/Order.cs
@@ -5,18 +5,38 @@ using System.ComponentModel;
 
 namespace CowboyCafe.Data
 {
-   public  class Order:INotifyPropertyChanged
+    public class Order : INotifyPropertyChanged
     {
+        private static uint lastOrdernumber = 0;
+        public Order(){
+            lastOrdernumber = lastOrdernumber + 1;
+        }
         public event PropertyChangedEventHandler PropertyChanged;
 
 
-        public double Subtotal => 0;
-        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+        private List<IOrderItem> items = new List<IOrderItem>();
+
+        public uint OrderNumber { get; } = lastOrdernumber + 1;
+
+        public double Subtotal {
+            get {
+                double total = 0;
+                foreach(IOrderItem item in items){
+                    total += item.Price;
+                }
+                return total;
+            }
+
+        }
+        public IEnumerable<IOrderItem> Items =>items;
             
         public void Add(IOrderItem item)
         {
-
+            if (item != null) { items.Add(item); }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
         }
-        public void Remove(IOrderItem item) { }
+        public void Remove(IOrderItem item) {
+            if (item != null) { items.Remove(item); }
+        }
    }
 }
